d9f4f5609b9214b09b8ad44ddbb288d4e05208b3
nwaackSnowflakeBadge <140658237+nwaackSnowflakeBadge@users.noreply.github.com>
2023-07-31 15:38:45 -0500

    Update streamlit_app.py
    Removed line outputting raw data and separated base url from the fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
 
 # normalize the json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
