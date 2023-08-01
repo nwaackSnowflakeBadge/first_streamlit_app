9325e24390cee31919d721d2df2a1a5f03bc5e50
nwaackSnowflakeBadge <140658237+nwaackSnowflakeBadge@users.noreply.github.com>
2023-08-01 09:38:09 -0500

    Update streamlit_app.py
    took 6 lines of code and re-distributed them so that we now have 1) a function that queries the table and 2) a button that calls our function and loads the data onto the page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,18 +41,22 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop() #don't run anything past here while we troubleshoot
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+#--Snowflake related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+
+#--Add a button to load the fruit
+if streamllit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
+  
 
 #add_a_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 #my_cur.execute("insert into fruit_load_list values ('" + add_a_fruit + "')")
 #streamlit.write('Thanks for adding ', add_a_fruit)
 
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values('from streamlit')")
