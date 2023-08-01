968c7e0b69c6c0eac0d923c518de1b25c6214e3c
nwaackSnowflakeBadge <140658237+nwaackSnowflakeBadge@users.noreply.github.com>
2023-07-31 19:59:20 -0500

    Update streamlit_app.py
    added jackfruit as default addition

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-add_a_fruit = streamlit.text_input('What fruit would you like to add?')
+add_a_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 my_cur.execute("insert into fruit_load_list values ('" + add_a_fruit + "')")
-streamlit.write('Thanks for adding ', fruit_choice)
+streamlit.write('Thanks for adding ', add_a_fruit)
