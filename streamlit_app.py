d37bc9b3aa4375b822fae428033ade3a372c7b4b
nwaackSnowflakeBadge <140658237+nwaackSnowflakeBadge@users.noreply.github.com>
2023-08-01 09:00:57 -0500

    Update streamlit_app.py
    stopped streamlit and added all imports to top

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -23,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-import requests
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -35,6 +37,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays normalized data in a table
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop() #don't run anything past here while we troubleshoot
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
