f10be283c7057d905ed1399a91d5a3147fb1237b
nwaackSnowflakeBadge <140658237+nwaackSnowflakeBadge@users.noreply.github.com>
2023-08-01 09:11:30 -0500

    Update streamlit_app.py
    added error checking and some flow to the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,27 +16,26 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+# --Let's put a pick list here so they can pick the fruit they want to include --
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
+# --Display the table on the page.--
 streamlit.dataframe(fruits_to_show)
 
 
-
-
-
+# --New Section to display fruityvice api response--
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize the json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays normalized data in a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information."
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop() #don't run anything past here while we troubleshoot
 
